8623cd8f1848c82abc6ea41a198cc1ef528104f8
sergio <sergioandresherreravallejo@gmail.com>
2024-05-02 00:21:30 -0500

    Quick sort functions implemented, need to investigate about QUICKSORT with two parameters

---
+++ pruebas.js
@@ -0,0 +1,100 @@
+function quickSort(A, p, r, mode) {
+  if (p < r) {
+    let q;
+    if (mode === "ascendente") {
+      q = partition(A, p, r, "ascendente");
+    } else if (mode === "descendente") {
+      q = partition(A, p, r, "descendente");
+    }
+    quickSort(A, p, q - 1, mode);
+    quickSort(A, q + 1, r, mode);
+  }
+}
+
+function partition(A, p, r, mode) {
+  let pivot;
+  if (mode === "descendente") {
+    pivot = A[r][1];
+  }
+
+  if (mode === "ascendente") {
+    pivot = A[r][2];
+  }
+
+  let i = p - 1;
+  let aux;
+
+  for (let j = p; j <= r - 1; j++) {
+    if (
+      (mode === "ascendente" && A[j][2] < pivot) ||
+      (mode === "descendente" && A[j][1] > pivot)
+    ) {
+      i++;
+      aux = A[j];
+      A[j] = A[i];
+      A[i] = aux;
+    }
+  }
+
+  aux = A[r];
+  A[r] = A[i + 1];
+  A[i + 1] = aux;
+  return i + 1;
+}
+
+let caso =
+  "Team1 1 5 C;Team2 5 10 C;Team3 2 15 I;Team1 3 20 R;Team2 1 25 R;Team2 4 30 C;Team1 3 35 I;Team1 3 40 C;Team3 2 45 I;Team3 2 50 C;Team5 5 10 C;Team5 9 10 C;Team5 6 900 C";
+
+function calcularScoreBoard(caso) {
+  caso = caso.split(";").map((x) => x.trim().split(" "));
+  let scoreboard = "";
+  let arr = [];
+
+  //Busca entre los 100 equipos
+  for (let j = 1; j <= 100; j++) {
+    // Filtra todos los arreglos que sean del equipo J
+    let FilteredArray = caso.filter((x) => x[0] === `Team${j}`);
+
+    let totalProblems = 0;
+    let totalTime = 0;
+
+    if (FilteredArray.length > 0) {
+      for (let i = 1; i < 10; i++) {
+        let problemas = FilteredArray.filter((x) => x[1] == i); // Filtrar los X que tengan ese problema
+
+        if (problemas.length > 0) {
+          let band = 0;
+          let incorrects = 0; // respuestas incorrectas
+          let correctTime = 0; // tiempo de la respuesta correcta
+          problemas.forEach((x) => {
+            if (x[3].toLowerCase() == "i" && band == 0) {
+              incorrects++;
+            }
+            if (x[3].toLowerCase() == "c" && band == 0) {
+              band = 1;
+              correctTime = parseInt(x[2]);
+              totalProblems++;
+            }
+          });
+          if (correctTime != 0) {
+            totalTime += incorrects * 20 + correctTime;
+          }
+        }
+      }
+      if (totalTime > 0) {
+        arr.push([`Team${j}`, totalProblems, totalTime]);
+      }
+    }
+
+    // quickSort(arr, 0, arr.length - 1, "desc");
+  }
+
+  return arr;
+}
+
+let a = calcularScoreBoard(caso);
+
+quickSort(a, 0, 3, "ascendente");
+quickSort(a, 0, 3, "descendente");
+
+console.log(a);
